46fe65668fee5ca05e1b22a2e7b5ea4bc2e835c2
Relop9000 <24LP9809@nest.edu.mn>
2025-03-17 21:05:30 +0800

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,21 @@
 const menuItems = {
   Food: [
-    { id: "21", name: "Meat Lovers", price: "35000" },
-    { id: "22", name: "BBQ Chicken", price: "35000" },
-    { id: "23", name: "Hawaii Pizza", price: "35000" },
+    { id: "21", name: "Meat Lovers", price: "35'0'" },
+    { id: "22", name: "BBQ Chicken", price: "35'0" },
+    { id: "23", name: "Hawaii Pizza", price: "35'0" },
   ],
-  Drinks: [
-    { id: "1", name: "Americano", price: "9000" },
-    { id: "2", name: "Cafe Latte", price: "9500" },
-    { id: "3", name: "Vanilla Latte", price: "10000" },
-    { id: "4", name: "Cafe Mocha", price: "10000" },
-    { id: "5", name: "Caramel Latte", price: "10000" },
-    { id: "6", name: "Green Tea Latte", price: "10500" },
-    { id: "7", name: "Green Tea", price: "15000" },
-    { id: "8", name: "Fruit Tea", price: "15000" },
-    { id: "9", name: "Earl Grey", price: "15000" },
+  Coffee: [
+    { id: "1", name: "Americano", price: "9'000" },
+    { id: "2", name: "Cafe Latte", price: "9'500" },
+    { id: "3", name: "Vanilla Latte", price: "10'000" },
+    { id: "4", name: "Cafe Mocha", price: "10'000" },
+    { id: "5", name: "Caramel Latte", price: "10'000" },
+    { id: "6", name: "Green Tea Latte", price: "10'500" },
+  ],
+  "Pot tea": [
+    { id: "7", name: "Green Tea", price: "15'000" },
+    { id: "8", name: "Fruit Tea", price: "15'000" },
+    { id: "9", name: "Earl Grey", price: "15'000" },
   ],
   Cocktails: [
     { id: "15", name: "Espresso Martini", price: "25000" },
@@ -25,10 +27,23 @@ const menuItems = {
   ],
   Beers: [
     { id: "10", name: "Heineken", price: "15000" },
-    { id: "11", name: "Kaltenberg", price: "15000" },
+    { id: "11", name: "Kaltenberg", price: "17000" },
     { id: "12", name: "Corona Extra", price: "17000" },
     { id: "13", name: "Kirin Ichiban", price: "15000" },
-    { id: "14", name: "Harbin", price: "20000" },
+    { id: "14", name: "Harbin", price: "20.0k" },
+    { id: "15", name: "Asahi", price: "15.0k" },
+  ],
+  Tequila: [
+    {
+      id: "14",
+      name: "Sierrai tequila",
+      price: "22.0k",
+      bottleprice: "170.0k",
+    },
+  ],
+  Liqueur: [
+    { id: "14", name: "Sierrai tequila", price: "22.0k", bottleprice: "" },
+    { id: "15", name: "Sierrai tequila", price: "22.0k", bottleprice: "" },
   ],
 };
 
